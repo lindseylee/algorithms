1ac030b98f3e0f2aafd6a2ebcddde0878de5876a
vagrant <vagrant@student-dev-box>
2014-06-11 16:20:10 -0500

    Array problem sets

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -1,13 +1,37 @@
 require 'rubygems'
-require 'pry-debugger'
+require 'pry-byebug'
 
 module ArrayUtil
-  def self.max(array)
-  end
+    def self.max(array)
+      if array.size == 0
+        return nil
+      end
+      
+      max = array.first
+      array.each do |x|
+        if x > max
+          max = x
+        end
+      end
+      max 
+    
+    end
 
-  def self.middle_element(array)
-  end
+    def self.middle_element(array)
+      if array.size == 0
+        nil
+      elsif array.size % 2 == 0
+        (array[array.size / 2]) + array[(array.size/2) -1].to_f / 2
+      else
+        array[array.size/ 2]
+      end
+    end
 
-  def self.sum_arrays(array1, array2)
-  end
+    def self.sum_arrays(array1, array2)
+      res = []
+      array1.size.times do |index|
+        res << (array1[index] + array2[index])
+      end
+      res
+    end
 end
--- set1/set1.rb
+++ set1/set1.rb
@@ -1,5 +1,11 @@
 module Set1
   def self.swap_small(array)
+    small_index = 0
+    array.each_index do |index|
+      if array[small_index] > array[index]
+        small_index = indexend
+      end
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
@@ -7,4 +13,5 @@ module Set1
 
   def self.find_sum_3(array)
   end
+
 end
