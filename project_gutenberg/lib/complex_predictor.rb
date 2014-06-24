92bd5cc559ad70d1ed14263b27f85a7abfcb082b
Lindsey Lee <lb.lee3@yahoo.com>
2014-06-24 16:04:49 -0500

    create basic predictor

--- project_gutenberg/lib/complex_predictor.rb
+++ project_gutenberg/lib/complex_predictor.rb
@@ -7,16 +7,37 @@ class ComplexPredictor < Predictor
   # Returns nothing.
   def train!
     @data = {}
-  end
+    counts = Hash.new
+
+    # @all_books.each do |category, impt_tokens|
+    #   @data[category] = {
+    #     impt_tokens: 0
+    #   }
+
+    def highest_token  
+      counts = Hash.new
+      @all_books.each do |category, books|
+        books.each do |filename, tokens|
+          tokens.each do |word|
+            counts[word] += 1
+          end
+        end
+      end
+    end
+
+    # books.each do |filename, tokens|
+    #   @data[category][:impt_tokens] += tokens.highest_token
+    # end
+  
 
   # Public: Predicts category.
   #
   # tokens - A list of tokens (words).
   #
   # Returns a category.
-  def predict(tokens)
-    # Always predict astronomy, for now.
-    :astronomy
+    def predict(tokens)
+      # Always predict astronomy, for now.
+      :astronomy
+    end
   end
 end
-
